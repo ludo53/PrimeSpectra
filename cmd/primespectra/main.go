2793140cb341d3dbccd36f7e47dbf45d2dc38ef3
Ludo <97764715+ludo53@users.noreply.github.com>
2025-08-02 20:48:59 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/primespectra/main.go
@@ -0,0 +1,23 @@
+// cmd/primespectra/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"primespectra/internal/primespectra"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := primespectra.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
